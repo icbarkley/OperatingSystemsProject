42c49957cb815808f03324161d64cb1c2917b3f6
icbarkley <117114274+icbarkley@users.noreply.github.com>
2023-11-28 22:33:48 -0600

    Create ProcessControlBlock.java

---
+++ ProcessControlBlock.java
@@ -0,0 +1,62 @@
+package com.company;
+
+public class ProcessControlBlock implements iProcessControlBlock {
+    private int id;
+    private String state; // dispatcher manage this
+    private int priority; // used in priority queue method
+    private String ioInfo; //Demonstrated the potential to upgrade, Will not be used in this system
+    private int burstTime; // used in round robin method
+
+    ProcessControlBlock() {
+        this.id = 0;
+        this.state = "";
+        this.priority = 0;
+        this.ioInfo = "";
+        this.burstTime = 0;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getIoInfo() {
+        return ioInfo;
+    }
+
+    public void setIoInfo(String ioInfo) {
+        this.ioInfo = ioInfo;
+    }
+
+    public int getBurstTime() {
+        return burstTime;
+    }
+
+    public void setBurstTime(int burstTime) {
+        this.burstTime = burstTime;
+    }
+
+    @Override
+    public void printLog() {
+        System.out.println("Process id: " + this.getId() + "\nProcess Priority" + this.getPriority());
+    }
+}
