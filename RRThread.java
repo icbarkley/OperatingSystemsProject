7fdd581950a790b814bd4d271c10f8f92ab7212e
icbarkley <117114274+icbarkley@users.noreply.github.com>
2023-11-28 22:34:48 -0600

    Create RRThread.java

---
+++ RRThread.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class RRThread implements Runnable {
+    Thread t;
+
+    public RRThread() {
+        Utilities.printHeadLine("In RR thread");
+        t = new Thread(this);
+        t.start();
+    }
+
+
+    @Override
+    public void run() {
+        try{
+            Utilities.printHeadLine("Start the OS");
+            OperatingSystem os = new OperatingSystem();
+            OperatingSystem.setMethod("RR");
+
+//        create a process
+            ProcessControlBlock pcb3 = new ProcessControlBlock();
+            pcb3.setId(3);
+//            pcb3.setPriority(6);
+            pcb3.setBurstTime(3);
+            Process time3= new CalculateProcess(pcb3);
+
+            ProcessControlBlock pcb4 = new ProcessControlBlock();
+            pcb4.setId(4);
+//            pcb4.setPriority(1);
+            pcb4.setBurstTime(2);
+            Process time4 = new CalculateProcess(pcb4);
+
+//        add it to the os
+            os.addNewProcess(time3);
+            os.addNewProcess(time4);
+            os.start();
+        } catch (Exception e) {
+            Utilities.printErr(e.getMessage());
+        }
+    }
+}
